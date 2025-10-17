b78120a6a777a5b58bde5a79367a18afb0c17318
AdinGreyling <adin.greyling@example.com>
2025-10-17 10:24:33 +0200

    Initial commit: Set up basic project foundation
    - Create basic Windows Forms application structure
    - Implement Hero class with core properties (ID, Name, Age, Superpower, ExamScore)
    - Add placeholder methods for future functionality
    - Set up basic form initialization
    - Prepare foundation for iterative development
    
    Next: Add and View functionality in Day 2

---
+++ Project_PRG282/Project_PRG282/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Project_PRG282
+{
+    public partial class Form1 : Form
+    {
+        // Variables
+        private string superheroesFile = "superheroes.txt";
+        private string summaryFile = "summary.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Basic form initialization
+            MessageBox.Show("Superhero Management System - Day 1 Setup Complete", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        // Placeholder methods for future development
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Add functionality - Coming in Day 2", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnViewAll_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("View functionality - Coming in Day 2", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Update functionality - Coming in Day 3", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Delete functionality - Coming in Day 3", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Report functionality - Coming in Day 3", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
---
+++ Project_PRG282/Project_PRG282/Hero.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Project_PRG282
+{
+    internal class Hero
+    {
+        // Basic Hero properties - Day 1 foundation
+        public string HeroID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Superpower { get; set; }
+        public int ExamScore { get; set; }
+        public string Rank { get; set; }
+        public string ThreatLevel { get; set; }
+
+        // Constructor for Day 1
+        public Hero()
+        {
+            HeroID = "";
+            Name = "";
+            Age = 0;
+            Superpower = "";
+            ExamScore = 0;
+            Rank = "";
+            ThreatLevel = "";
+        }
+
+        // Basic constructor with parameters
+        public Hero(string id, string name, int age, string superpower, int examScore)
+        {
+            HeroID = id;
+            Name = name;
+            Age = age;
+            Superpower = superpower;
+            ExamScore = examScore;
+            Rank = ""; // Will be calculated in future days
+            ThreatLevel = ""; // Will be calculated in future days
+        }
+    }
+}
